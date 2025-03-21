e3836766819cdfa5be2698d2098a46fc232e0dba
sebastian <wzseba@gmail.com>
2025-03-20 23:00:16 -0300

    remove: login inicial al levantar el servidor

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.formLogin(login -> login.disable());
+
+		return http.build();
+	}
+}
